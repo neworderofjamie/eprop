c2fedca1770e6b90ad5610524f0cda1bd91b95be
neworderofjamie <neworderofjamie@gmail.com>
2020-07-16 14:20:33 +0100

    Perform initial tranpose

--- pattern_recognition_1_1/simulator.cc
+++ pattern_recognition_1_1/simulator.cc
@@ -1,14 +1,34 @@
 #include "pattern_recognition_1_1_CODE/definitions.h"
 
+#include <iostream>
 // GeNN userproject includes
 #include "analogueRecorder.h"
 #include "spikeRecorder.h"
+
+// EProp includes
+#include "transpose.h"
+
 
 int main()
 {
     allocateMem();
     initialize();
+    
+    // Use CUDA to calculate initial transpose of feedforward recurrent->output weights
+    BatchLearning::transposeCUDA(d_gRecurrentOutput, d_gOutputRecurrent, 600, 3);
+
     initializeSparse();
+    
+    // **TEMP** test transpose
+    /*pullgRecurrentOutputFromDevice();
+    pullgOutputRecurrentFromDevice();
+    for(unsigned int i = 0; i < 600; i++) {
+        for(unsigned int j = 0; j < 3; j++) {
+            std::cout << gRecurrentOutput[(i * 3) + j] << ", ";
+            assert(gRecurrentOutput[(i * 3) + j] == gOutputRecurrent[(j * 600) + i]);
+        }
+        std::cout << std::endl;
+    }*/
     
     SpikeRecorder<SpikeWriterTextCached> inputSpikeRecorder(&getInputCurrentSpikes, &getInputCurrentSpikeCount, "input_spikes.csv", ",", true);
     SpikeRecorder<SpikeWriterTextCached> recurrentSpikeRecorder(&getRecurrentCurrentSpikes, &getRecurrentCurrentSpikeCount, "recurrent_spikes.csv", ",", true);
